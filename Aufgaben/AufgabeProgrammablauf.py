3a27ad3c23630505114340bd9e037ef9715d49bf
Martulinka <martazak468@gmail.com>
2025-05-12 10:58:15 +0000

    .

---
+++ Aufgaben/AufgabeProgrammablauf.py
@@ -0,0 +1,9 @@
+kartenstapel = ["gr7", "gl2" , "+4"]
+while len(kartenstapel) !=0 :
+    print(kartenstapel)
+    aktuelleKarte = kartenstapel.pop(0)
+    if aktuelleKarte == +4 :
+        print("+4 kann gespielt werden")
+        break 
+if len(kartenstapel) ==0:
+    print("Stapel ist leer")
