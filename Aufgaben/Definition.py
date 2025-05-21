1d9f5653ea159b9d2e5c15c64701830b2ba42541
Martulinka <martazak468@gmail.com>
2025-05-21 13:58:26 +0000

    .

--- Aufgaben/Definition.py
+++ Aufgaben/Definition.py
@@ -1,6 +1,5 @@
-def printhello(i:int):
+def print_hello_world(i):
     print(f"Hello World {i}")
     return 4
-
-printhello(5)       
-printhello("t")
+print_hello_world(5)       
+print_hello_world("t")
---
+++ Aufgaben/testDef.py
@@ -0,0 +1,4 @@
+import Definition
+from Definition import print_hello_world        
+4=print_hello_world(5)
+print(4)
