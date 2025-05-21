4d613d4dcd3d039438f2150b09317a9685972db5
Martulinka <martazak468@gmail.com>
2025-05-21 13:45:58 +0000

    .

---
+++ Aufgaben/Definition.py
@@ -0,0 +1,6 @@
+def printhello(i:int):
+    print(f"Hello World {i}")
+    return 4
+
+printhello(5)       
+printhello("t")
--- Aufgaben/Leiterspiel.py
+++ Aufgaben/Leiterspiel.py
@@ -2,39 +2,12 @@ Spielbrett = {0: "Start", 3: "Leiter nach oben 10", 5: "Schlange nach unten 3", 
 Würfeln = (1, 2, 3, 4, 5, 6)
 import random
 Position = 0
+anzahl_würfe = 0
 while Position < 100:
+            anzahl_würfe+=1
             Würfelwurf = random.choice(Würfeln)
             print(Würfelwurf)
-if Position + random.choice(Würfeln) == Spielbrett[3]:
-		print("Du bist auf dem Feld 3, die Leiter befördert dich um 10 Felder nach oben")
-		Position = Position + 10
-		if Position + random.choice(Würfeln) == Spielbrett[5]:
-                         print("Du bist auf dem Feld 5, die Schlange befördert dich um 3 Felder nach unten")
-Position = Position - 3
-if Position + random.choice(Würfeln) == Spielbrett[8]:
-            print("Du bist auf dem Feld 8, die Leiter befördert dich um 22 Felder nach oben")
-            Position = Position + 22
-if Position + random.choice(Würfeln) == Spielbrett[10]:
-            print("Du bist auf dem Feld 10, die Schlange befördert dich um 5 Felder nach unten")
-            Position = Position - 5
-            if Position + random.choice(Würfeln) == Spielbrett[20]:
-                    print("Du bist auf dem Feld 20, die Leiter befördert dich um 15 Felder nach oben")
-            Position = Position + 15    
-            if Position + random.choice(Würfeln) == Spielbrett[24]:
-                    print("Du bist auf dem Feld 24, die Schlange befördert dich um 10 Felder nach unten")       
-            Position = Position - 10
-            if Position + random.choice(Würfeln) == Spielbrett[40]:
-                    print("Du bist auf dem Feld 40, die Leiter befördert dich um 20 Felder nach oben")      
-            Position = Position + 20        
-            if Position + random.choice(Würfeln) == Spielbrett[45]:
-                    print("Du bist auf dem Feld 45, die Schlange befördert dich um 15 Felder nach unten")       
-            Position = Position - 15    
-            if Position + random.choice(Würfeln) == Spielbrett[98]:
-                    print("Du bist auf dem Feld 98, die Schlange befördert dich um 20 Felder nach unten")   
-            Position = Position - 20    
-            if Position + random.choice(Würfeln) == Spielbrett[100]:
-                    print("Du bist auf dem Feld 100, du hast das Ziel erreicht")
-                    continue
+            Position=Position+Würfelwurf+Spielbrett[Position+Würfelwurf]      
 if Position + random.choice(Würfeln) > 100:
-        print("Du bist auf dem Feld 100, du hast das Ziel erreicht")
+        print("Du bist auf dem Feld 100, du hast das Ziel erreicht")    
         
